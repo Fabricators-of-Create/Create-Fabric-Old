4d1c80ae7b6a3db09410367f012c7527be5c3cbe
smellypengu <43011139+smellypengu@users.noreply.github.com>
2021-03-07 00:04:42 +0100

    Moved fluid renderer code from CreateClient to AllFluids temporarily for cleaner code.

--- src/main/java/com/smellypengu/createfabric/CreateClient.java
+++ src/main/java/com/smellypengu/createfabric/CreateClient.java
@@ -14,27 +14,11 @@ import com.smellypengu.createfabric.foundation.render.backend.Backend;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler;
-import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.SpriteAtlasTexture;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.FluidState;
 import net.minecraft.resource.ReloadableResourceManager;
 import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourceType;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.BlockRenderView;
-
-import java.util.function.Function;
 
 public class CreateClient implements ClientModInitializer {
     public static KineticRenderer kineticRenderer;
@@ -58,6 +42,7 @@ public class CreateClient implements ClientModInitializer {
         AllKeys.register();
         AllTileEntities.registerRenderers();
         AllEntityTypes.registerRenderers();
+        AllFluids.registerRenderers();
 
         ResourceManager resourceManager = MinecraftClient.getInstance()
                 .getResourceManager();
@@ -72,62 +57,8 @@ public class CreateClient implements ClientModInitializer {
 
 
 
-        setupFluidRendering(AllFluids.STILL_CHOCOLATE, AllFluids.FLOWING_CHOCOLATE, new Identifier("create", "chocolate"), 0x00ffffff);
-        BlockRenderLayerMap.INSTANCE.putFluids(RenderLayer.getTranslucent(), AllFluids.STILL_CHOCOLATE, AllFluids.FLOWING_CHOCOLATE);
-
-        setupFluidRendering(AllFluids.STILL_HONEY, AllFluids.FLOWING_HONEY, new Identifier("create", "honey"), 0x00ffffff);
-        BlockRenderLayerMap.INSTANCE.putFluids(RenderLayer.getTranslucent(), AllFluids.STILL_HONEY, AllFluids.FLOWING_HONEY);
 
         BlockRenderLayerMap.INSTANCE.putBlock(AllPaletteBlocks.TILED_GLASS, RenderLayer.getCutout());
-    }
-
-    public static void setupFluidRendering(final Fluid still, final Fluid flowing, final Identifier textureFluidId, final int color) {
-        final Identifier stillSpriteId = new Identifier(textureFluidId.getNamespace(), "fluid/" + textureFluidId.getPath() + "_still");
-        final Identifier flowingSpriteId = new Identifier(textureFluidId.getNamespace(), "fluid/" + textureFluidId.getPath() + "_flow");
-
-        // If they're not already present, add the sprites to the block atlas
-        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE).register((atlasTexture, registry) -> {
-            registry.register(stillSpriteId);
-            registry.register(flowingSpriteId);
-        });
-
-        final Identifier fluidId = Registry.FLUID.getId(still);
-        final Identifier listenerId = new Identifier(fluidId.getNamespace(), fluidId.getPath() + "_reload_listener");
-
-        final Sprite[] fluidSprites = { null, null };
-
-        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
-            @Override
-            public Identifier getFabricId() {
-                return listenerId;
-            }
-
-            /**
-             * Get the sprites from the block atlas when resources are reloaded
-             */
-            @Override
-            public void apply(ResourceManager resourceManager) {
-                final Function<Identifier, Sprite> atlas = MinecraftClient.getInstance().getSpriteAtlas(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE);
-                fluidSprites[0] = atlas.apply(stillSpriteId);
-                fluidSprites[1] = atlas.apply(flowingSpriteId);
-            }
-        });
-
-        // The FluidRenderer gets the sprites and color from a FluidRenderHandler during rendering
-        final FluidRenderHandler renderHandler = new FluidRenderHandler() {
-            @Override
-            public Sprite[] getFluidSprites(BlockRenderView view, BlockPos pos, FluidState state) {
-                return fluidSprites;
-            }
-
-            @Override
-            public int getFluidColor(BlockRenderView view, BlockPos pos, FluidState state) {
-                return color;
-            }
-        };
-
-        FluidRenderHandlerRegistry.INSTANCE.register(still, renderHandler);
-        FluidRenderHandlerRegistry.INSTANCE.register(flowing, renderHandler);
     }
 
     public static CustomBlockModels getCustomBlockModels() {
