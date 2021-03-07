5e85c3d446e645ea33cb2a9fa08172e75d8945e2
smellypengu <43011139+smellypengu@users.noreply.github.com>
2021-03-07 21:53:20 +0100

    More of Registrate

---
+++ src/main/java/com/smellypengu/registrate/util/NonNullLazyValue.java
@@ -0,0 +1,18 @@
+package com.smellypengu.registrate.util;
+
+import com.smellypengu.registrate.util.nullness.NonNullSupplier;
+import com.smellypengu.registrate.util.nullness.NonnullType;
+
+import net.minecraft.util.Lazy;
+
+public class NonNullLazyValue<T> extends Lazy<T> implements NonNullSupplier<T> {
+
+    public NonNullLazyValue(NonNullSupplier<T> supplier) {
+        super(supplier);
+    }
+
+    @Override
+    public @NonnullType T get() {
+        return get();
+    }
+}
