d5cc22a3c082de681a380c18e533e22cdf976dba
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-03-10 16:52:24 +0200

    Should be good now

--- src/main/java/com/simibubi/create/foundation/mixin/Vec3dMixin.java
+++ src/main/java/com/simibubi/create/foundation/mixin/Vec3dMixin.java
@@ -1,7 +1,6 @@
 package com.simibubi.create.foundation.mixin;
 
 import net.minecraft.util.math.Vec3d;
-import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -12,12 +11,10 @@ public abstract class Vec3dMixin {
 
 	// They are client-only, but not anymore!
 
-	@Intrinsic
 	public Vec3d negate() {
 		return multiply(-1.0D);
 	}
 
-	@Intrinsic
 	public Vec3d method_22882() {
 		return negate();
 	}
