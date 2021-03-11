655bf7561885821c8b36a12e6fc9c97329f85e0e
YTG123 <54103478+YTG1234@users.noreply.github.com>
2021-03-11 16:03:35 +0200

    Inner classes of inner classes YEET

--- src/main/java/com/simibubi/create/foundation/resource/AllResources.java
+++ src/main/java/com/simibubi/create/foundation/resource/AllResources.java
@@ -14,103 +14,109 @@ import static net.devtech.arrp.json.lang.JLang.lang;
 public final class AllResources {
 	public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create(id(ID).toString());
 
-	private static JLang addLang(JLang lang) {
-		ConfigLang.addAll(lang);
-		BlockLang.addAll(lang);
-
-		return lang;
-	}
-
-	private static final class ConfigLang {
-		public static void addAll(JLang lang) {
-			addClient(lang);
-			addCommon(lang);
+	private static final class TranslationHandler {
+		public static void addLang(RuntimeResourcePack rp) {
+			rp.addLang(id("en_us"), addLang(lang()));
 		}
 
-		private static void addClient(JLang lang) {
-			// Option Names
-			configText(lang, "client", "Client");
-			configText(lang, "enableTooltips", "Enable Tooltips", "client");
+		private static JLang addLang(JLang lang) {
+			ConfigLang.addAll(lang);
+			BlockLang.addAll(lang);
 
-			// Tooltips
-			configTooltip(lang, "client", "Client-only settings - If you're looking for general settings, look inside your worlds serverconfig folder!");
-			configTooltip(lang, "enableTooltips", "Show item descriptions on Shift and controls on Ctrl.", "client");
+			return lang;
 		}
 
-		private static void addCommon(JLang lang) {
-			configText(lang, "common", "Common");
-		}
-
-		private static String getOptionKey(String option, String... parents) {
-			StringBuilder str = new StringBuilder("text.autoconfig." + ID + ".option");
-			if (parents.length > 0) {
-				Arrays.stream(parents).forEach(parent -> str.append(".").append(parent));
-			}
-			str.append(".").append(option);
-
-			return str.toString();
-		}
-
-		private static void configText(JLang lang, String option, String name, String... parents) {
-			lang.entry(getOptionKey(option, parents), name);
-		}
-
-		private static void configTooltip(JLang lang, String option, String tooltip, String... parents) {
-			String key = getOptionKey(option, parents) + ".@Tooltip";
-
-			String[] split = key.split("\n");
-			if (split.length == 1) {
-				lang.entry(key, split[0]);
-				return;
+		private static final class ConfigLang {
+			public static void addAll(JLang lang) {
+				addClient(lang);
+				addCommon(lang);
 			}
 
-			for (int i = 0; i < split.length; i++) {
-				lang.entry(key + "[" + i + "]", split[i]);
+			private static void addClient(JLang lang) {
+				// Option Names
+				configText(lang, "client", "Client");
+				configText(lang, "enableTooltips", "Enable Tooltips", "client");
+
+				// Tooltips
+				configTooltip(lang, "client", "Client-only settings - If you're looking for general settings, look inside your worlds serverconfig folder!");
+				configTooltip(lang, "enableTooltips", "Show item descriptions on Shift and controls on Ctrl.", "client");
+			}
+
+			private static void addCommon(JLang lang) {
+				configText(lang, "common", "Common");
+			}
+
+			private static String getOptionKey(String option, String... parents) {
+				StringBuilder str = new StringBuilder("text.autoconfig." + ID + ".option");
+				if (parents.length > 0) {
+					Arrays.stream(parents).forEach(parent -> str.append(".").append(parent));
+				}
+				str.append(".").append(option);
+
+				return str.toString();
+			}
+
+			private static void configText(JLang lang, String option, String name, String... parents) {
+				lang.entry(getOptionKey(option, parents), name);
+			}
+
+			private static void configTooltip(JLang lang, String option, String tooltip, String... parents) {
+				String key = getOptionKey(option, parents) + ".@Tooltip";
+
+				String[] split = key.split("\n");
+				if (split.length == 1) {
+					lang.entry(key, split[0]);
+					return;
+				}
+
+				for (int i = 0; i < split.length; i++) {
+					lang.entry(key + "[" + i + "]", split[i]);
+				}
 			}
 		}
-	}
 
-	// Consider: The following? Will not be able to copy Forge Create lang when it updates
+		// Consider: The following? Will not be able to copy Forge Create lang when it updates
 
-	private static final class BlockLang {
-		public static void addAll(JLang lang) {
-			lang.block(id("acacia_window"), "Acacia Window");
-			lang.block(id("acacia_window_pane"), "Acacia Window Pane");
+		private static final class BlockLang {
+			public static void addAll(JLang lang) {
+				lang.block(id("acacia_window"), "Acacia Window");
+				lang.block(id("acacia_window_pane"), "Acacia Window Pane");
 
-			// TODO: All other blocks
+				// TODO: All other blocks
+			}
 		}
+
+		// TODO: Item lang
+		// TODO: Advancement lang
+		// TODO: Item group lang
+		// TODO: Death lang
+
+		// TODO: Create recipe lang
+		// TODO: Create generic lang
+		// TODO: Create action lang
+		// TODO: Create keyinfo lang
+		// TODO: Create GUI lang
+		// TODO: Create symmetry lang
+		// TODO: Create orientation lang
+		// TODO: Create terrainzapper lang
+		// TODO: Create blockzapper lang
+		// TODO: Create minecart coupling lang
+		// TODO: Create contraptions lang
+		// TODO: Create logistics lang
+		// TODO: Create schematic and quill lang
+		// TODO: Create schematic lang
+		// TODO: Create material checklist lang
+		// TODO: Create item attributes lang
+		// TODO: Create tooltip lang
+		// TODO: Create mechanical arm lang
+		// TODO: Create tunnel lang
+		// TODO: Create hint lang
+		// TODO: Create command lang
+		// TODO: Create subtitle lang
 	}
-
-	// TODO: Item lang
-	// TODO: Advancement lang
-	// TODO: Item group lang
-	// TODO: Death lang
-
-	// TODO: Create recipe lang
-	// TODO: Create generic lang
-	// TODO: Create action lang
-	// TODO: Create keyinfo lang
-	// TODO: Create GUI lang
-	// TODO: Create symmetry lang
-	// TODO: Create orientation lang
-	// TODO: Create terrainzapper lang
-	// TODO: Create blockzapper lang
-	// TODO: Create minecart coupling lang
-	// TODO: Create contraptions lang
-	// TODO: Create logistics lang
-	// TODO: Create schematic and quill lang
-	// TODO: Create schematic lang
-	// TODO: Create material checklist lang
-	// TODO: Create item attributes lang
-	// TODO: Create tooltip lang
-	// TODO: Create mechanical arm lang
-	// TODO: Create tunnel lang
-	// TODO: Create hint lang
-	// TODO: Create command lang
-	// TODO: Create subtitle l<ng
 
 	private static void addResources(RuntimeResourcePack pack) {
-		pack.addLang(id("en_us"), addLang(lang()));
+		TranslationHandler.addLang(pack);
 	}
 
 	public static void initialize() {
