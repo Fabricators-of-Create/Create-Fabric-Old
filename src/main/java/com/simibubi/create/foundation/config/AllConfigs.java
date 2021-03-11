6f4647cf5a4f98b3c461b8e5c612a8f91d34acc6
Tropheus Jay <jverrellijr@icloud.com>
2021-03-11 13:37:19 -0500

    This is not done I'm just pushing what I have to compare

--- src/main/java/com/simibubi/create/foundation/config/AllConfigs.java
+++ src/main/java/com/simibubi/create/foundation/config/AllConfigs.java
@@ -9,14 +9,11 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.text.TranslatableText;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Config(name = "create")
 public class AllConfigs implements ConfigData {
-
-	public static void register() {
-		AutoConfig.register(AllConfigs.class, Toml4jConfigSerializer::new);
-	}
-
 	// use this to use the config
 	// ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 
@@ -53,4 +50,60 @@ public class AllConfigs implements ConfigData {
 	CStress stress = new CStress();
 	@ConfigEntry.Gui.CollapsibleObject
 	CWorldGen worldGen = new CWorldGen();
+
+
+
+	public static void register() {
+		validateConfigs();
+		AutoConfig.register(AllConfigs.class, Toml4jConfigSerializer::new);
+	}
+
+	// MY EYES
+	public static void validateConfigs() { // this is probably the wrong way to do it but post-validation documentation is literally 3 sentences
+		final Logger LOGGER = LogManager.getLogger("Create Config Validation");
+		String reset = " reset! Invalid Value!";
+		if (!(CClient.fanParticleDensity >= 0) ||
+			(CClient.fanParticleDensity <= 1)) 				{ CClient.fanParticleDensity = 0.5f; 		LOGGER.warn("fanParticleDensity" + reset);}
+		if (!(CClient.overlayOffsetX >= Integer.MIN_VALUE)) { CClient.overlayOffsetX = 0; 				LOGGER.warn("overlayOffsetX" + reset);} // i know what the errors say. gonna leave it anyway.
+		if (!(CClient.overlayOffsetY >= Integer.MIN_VALUE)) { CClient.overlayOffsetY = 0; 				LOGGER.warn("overlayOffsetY" + reset);}
+		if (!(CCuriosities.maxSymmetryWandRange > 10)) 		{ CCuriosities.maxSymmetryWandRange = 50; 	LOGGER.warn("maxSymmetryWandRange" + reset);}
+		//if (!(CCuriosities.zapperUndoLogLength > 0)) 		{ CCuriosities.zapperUndoLogLength = 10; 	LOGGER.warn("zapperUndoLogLength" + reset");}
+		if (!(CFluids.fluidTankCapacity >= 1)) 				{ CFluids.fluidTankCapacity = 8; 			LOGGER.warn("fluidTankCapacity" + reset);}
+		if (!(CFluids.fluidTankMaxHeight >= 1)) 			{ CFluids.fluidTankMaxHeight = 32; 			LOGGER.warn("fluidTankCapacity" + reset);}
+		if (!(CFluids.mechanicalPumpRange >= 1)) 			{ CFluids.mechanicalPumpRange = 16; 		LOGGER.warn("mechanicalPumpRange" + reset);}
+		if (!(CFluids.hosePulleyBlockThreshold >= -1)) 		{ CFluids.hosePulleyBlockThreshold = 10000; LOGGER.warn("hosePulleyBlockThreshold" + reset);}
+		if (!(CFluids.hosePulleyRange >= 1)) 				{ CFluids.hosePulleyRange = 128; 			LOGGER.warn("hosePulleyRange" + reset);}
+		if (!(CKinetics.maxBeltLength >= 5)) 				{ CKinetics.maxBeltLength = 20; 			LOGGER.warn("maxBeltLength" + reset);}
+		if (!(CKinetics.crushingDamage >= 0)) 				{ CKinetics.crushingDamage = 4; 			LOGGER.warn("crushingDamage" + reset);}
+		if (!(CKinetics.maxMotorSpeed >= 64)) 				{ CKinetics.maxMotorSpeed = 256; 			LOGGER.warn("maxMotorSpeed" + reset);}
+		if (!(CKinetics.waterWheelBaseSpeed >= 1)) 			{ CKinetics.waterWheelBaseSpeed = 4; 		LOGGER.warn("waterWheelBaseSpeed" + reset);}
+		if (!(CKinetics.waterWheelFlowSpeed >= 1)) 			{ CKinetics.waterWheelFlowSpeed = 4; 		LOGGER.warn("waterWheelFlowSpeed" + reset);}
+		if (!(CKinetics.furnaceEngineSpeed >= 1)) 			{ CKinetics.furnaceEngineSpeed = 16; 		LOGGER.warn("furnaceEngineSpeed" + reset);}
+		if (!(CKinetics.maxRotationSpeed >= 64)) 			{ CKinetics.maxRotationSpeed = 256; 		LOGGER.warn("maxRotationSpeed" + reset);}
+		if (!(CKinetics.kineticValidationFrequency >= 5)) 	{ CKinetics.kineticValidationFrequency = 60;LOGGER.warn("kineticValidationFrequency" + reset);}
+		if (!(CKinetics.crankHungerMultiplier >= 0) ||
+			(CKinetics.crankHungerMultiplier <= 1)) 		{ CKinetics.crankHungerMultiplier = 0.01f; 	LOGGER.warn("crankHungerMultiplier" + reset);}
+		if (!(CKinetics.fanPushDistance >= 5)) 				{ CKinetics.fanPushDistance = 20;			LOGGER.warn("fanPushDistance" + reset);}
+		if (!(CKinetics.fanPullDistance >= 5)) 				{ CKinetics.fanPullDistance = 20;			LOGGER.warn("fanPullDistance" + reset);}
+		if (!(CKinetics.fanBlockCheckRate >= 10)) 			{ CKinetics.fanBlockCheckRate = 30;			LOGGER.warn("fanBlockCheckRate" + reset);}
+		if (!(CKinetics.fanRotationArgmax >= 64)) 			{ CKinetics.fanRotationArgmax = 256;		LOGGER.warn("fanRotationArgmax" + reset);}
+		if (!(CKinetics.generatingFanSpeed >= 0)) 			{ CKinetics.generatingFanSpeed = 4;			LOGGER.warn("generatingFanSpeed" + reset);}
+		if (!(CKinetics.maxBlocksMoved >= 1)) 				{ CKinetics.maxBlocksMoved = 2048;			LOGGER.warn("maxBlocksMoved" + reset);}
+		if (!(CKinetics.maxChassisRange >= 1)) 				{ CKinetics.maxChassisRange = 16;			LOGGER.warn("maxChassisRange" + reset);}
+		if (!(CKinetics.maxPistonPoles >= 1)) 				{ CKinetics.maxPistonPoles = 64;			LOGGER.warn("maxPistonPoles" + reset);}
+		if (!(CKinetics.maxRopeLength >= 1)) 				{ CKinetics.maxRopeLength = 128;			LOGGER.warn("maxRopeLength" + reset);}
+		if (!(CKinetics.maxCartCouplingLength >= 1)) 		{ CKinetics.maxCartCouplingLength = 32;		LOGGER.warn("maxCartCouplingLength" + reset);}
+		/*if (!(CKinetics.crankHungerMultiplier >= 0) ||
+			(CKinetics.crankHungerMultiplier <= 1)) 		{ CKinetics.crankHungerMultiplier = 0.01f; 	LOGGER.warn("crankHungerMultiplier" + reset);}
+		if (!(CKinetics.crankHungerMultiplier >= 0) ||
+			(CKinetics.crankHungerMultiplier <= 1)) 		{ CKinetics.crankHungerMultiplier = 0.01f; 	LOGGER.warn("crankHungerMultiplier" + reset);}
+		if (!(CKinetics.crankHungerMultiplier >= 0) ||
+			(CKinetics.crankHungerMultiplier <= 1)) 		{ CKinetics.crankHungerMultiplier = 0.01f; 	LOGGER.warn("crankHungerMultiplier" + reset);}
+		if (!(CKinetics.crankHungerMultiplier >= 0) ||
+			(CKinetics.crankHungerMultiplier <= 1)) 		{ CKinetics.crankHungerMultiplier = 0.01f; 	LOGGER.warn("crankHungerMultiplier" + reset);}
+		if (!(CKinetics.crankHungerMultiplier >= 0) ||
+			(CKinetics.crankHungerMultiplier <= 1)) 		{ CKinetics.crankHungerMultiplier = 0.01f; 	LOGGER.warn("crankHungerMultiplier" + reset);}
+		if (!(CKinetics.crankHungerMultiplier >= 0) ||
+			(CKinetics.crankHungerMultiplier <= 1)) 		{ CKinetics.crankHungerMultiplier = 0.01f; 	LOGGER.warn("crankHungerMultiplier" + reset);}*/
+	}
 }
--- src/main/java/com/simibubi/create/foundation/config/CFluids.java
+++ src/main/java/com/simibubi/create/foundation/config/CFluids.java
@@ -1,9 +1,9 @@
 package com.simibubi.create.foundation.config;
 
 public class CFluids {
-	int fluidTankCapacity = 8; // min 1, "[in Buckets]" "The amount of liquid a tank can hold per block."
-	int fluidTankMaxHeight = 32; // min 1, "[in Blocks]" "The maximum height a fluid tank can reach."
-	int mechanicalPumpRange = 16; // min 1, "[in Blocks]" "The maximum distance a mechanical pump can push or pull liquids on either side."
-	int hosePulleyBlockThreshold = 10000; // min -1, "[in Blocks]" "[-1 to disable this behaviour]" "The minimum amount of fluid blocks the hose pulley needs to find before deeming it an infinite source."
-	int hosePulleyRange = 128; // min 1, "[in Blocks]" "The maximum distance a hose pulley can draw fluid blocks from."
+	static int fluidTankCapacity = 8; // min 1, "[in Buckets]" "The amount of liquid a tank can hold per block."
+	static int fluidTankMaxHeight = 32; // min 1, "[in Blocks]" "The maximum height a fluid tank can reach."
+	static int mechanicalPumpRange = 16; // min 1, "[in Blocks]" "The maximum distance a mechanical pump can push or pull liquids on either side."
+	static int hosePulleyBlockThreshold = 10000; // min -1, "[in Blocks]" "[-1 to disable this behaviour]" "The minimum amount of fluid blocks the hose pulley needs to find before deeming it an infinite source."
+	static int hosePulleyRange = 128; // min 1, "[in Blocks]" "The maximum distance a hose pulley can draw fluid blocks from."
 }
