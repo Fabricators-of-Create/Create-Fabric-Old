8a23b37af3b941890ef641996b9e1ec5f147aa11
Tropheus Jay <jverrellijr@icloud.com>
2021-03-11 11:38:57 -0500

    it's ugly but it works all configs should now be implemented waiting on lang to make it pretty

--- src/main/java/com/simibubi/create/foundation/config/CSchematics.java
+++ src/main/java/com/simibubi/create/foundation/config/CSchematics.java
@@ -1,11 +1,14 @@
 package com.simibubi.create.foundation.config;
 
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
 public class CSchematics {
 	int maxSchematics = 10; // min 1, "The amount of Schematics a player can upload until previous ones are overwritten."
 	int maxTotalSchematicSize = 256; // min 16, "[in KiloBytes]" "The maximum allowed file size of uploaded Schematics."
 	int maxSchematicPacketSize = 1024; // min 256, max 32767, "[in Bytes]" "The maximum packet size uploaded Schematics are split into."
 	int schematicIdleTimeout = 600; // min 100, "Amount of game ticks without new packets arriving until an active schematic upload process is discarded."
 	// schematicannon group         public ConfigGroup schematicannon = group(0, "schematicannon", "Schematicannon");
+	@ConfigEntry.Gui.PrefixText()
 	int schematicannonDelay = 10; // min 1, "Amount of game ticks between shots of the cannon. Higher => Slower"
 	int schematicannonSkips = 10; // min 1, "Amount of block positions per tick scanned by a running cannon. Higher => Faster"
 	float schematicannonGunpowderWorth = 20f; // min 0, max 100, "% of Schematicannon's Fuel filled by 1 Gunpowder."
--- src/main/java/com/simibubi/create/foundation/config/CStress.java
+++ src/main/java/com/simibubi/create/foundation/config/CStress.java
@@ -1,55 +1,50 @@
 package com.simibubi.create.foundation.config;
 
 public class CStress {
-	/* todo: oh no
-	private Map<Identifier, ConfigValue<Double>> capacities = new HashMap<>();
-	private Map<Identifier, ConfigValue<Double>> impacts = new HashMap<>();
-
-	@Override
-	protected void registerAll(Builder builder) {
-		builder.comment("", Comments.su, Comments.impact)
-			.push("impact");
-		StressConfigDefaults.registeredDefaultImpacts
-			.forEach((r, i) -> getImpacts().put(r, builder.define(r.getPath(), i)));
-		builder.pop();
-
-		builder.comment("", Comments.su, Comments.capacity)
-			.push("capacity");
-		StressConfigDefaults.registeredDefaultCapacities
-			.forEach((r, i) -> getCapacities().put(r, builder.define(r.getPath(), i)));
-		builder.pop();
-	}
-
-	public double getImpactOf(Block block) {
-		Identifier key = block.getRegistryName();
-		return getImpacts().containsKey(key) ? getImpacts().get(key)
-			.get() : 0;
-	}
-
-	public double getCapacityOf(Block block) {
-		Identifier key = block.getRegistryName();
-		return getCapacities().containsKey(key) ? getCapacities().get(key)
-			.get() : 0;
-	}
-
-	@Override
-	public String getName() {
-		return "stressValues.v" + StressConfigDefaults.forcedUpdateVersion;
-	}
-
-	public Map<Identifier, ConfigValue<Double>> getImpacts() {
-		return impacts;
-	}
-
-	public Map<Identifier, ConfigValue<Double>> getCapacities() {
-		return capacities;
-	}
-
-	private static class Comments {
-		static String su = "[in Stress Units]";
-		static String impact =
-			"Configure the individual stress impact of mechanical blocks. Note that this cost is doubled for every speed increase it receives.";
-		static String capacity = "Configure how much stress a source can accommodate for.";
-	}
-	*/
+	// "[in Stress Units]"
+	// "Configure the individual stress impact of mechanical blocks. Note that this cost is doubled for every speed increase it receives."
+	float mechanicalBearing = 4.0f;
+	float clockworkBearing = 4.0f;
+	float turntable = 4.0f;
+	float sequencedGearshift = 0.0f;
+	float stickyMechanicalPiston = 4.0f;
+	float crushingWheel = 8.0f;
+	float mechanicalArm = 8.0f;
+	float ropePulley = 4.0f;
+	float mechanicalCrafter = 2.0f;
+	float mechanicalDrill = 4.0f;
+	float flywheel = 0.0f;
+	float hosePulley = 4.0f;
+	float mechanicalSaw = 4.0f;
+	float largeCogwheel = 0.0f;
+	float deployer = 4.0f;
+	float gearbox = 0.0f;
+	float mysteriousCuckooClock = 1.0f;
+	float stressometer = 0.0f;
+	float encasedChainDrive = 0.0f;
+	float rotationSpeedController = 0.0f;
+	float belt = 1.0f;
+	float cogwheel = 0.0f;
+	float mechanicalPump = 4.0f;
+	float mechanicalMixer = 4.0f;
+	float andesiteEncasedShaft = 0.0f;
+	float adjustableChainGearshift = 0.0f;
+	float mechanicalPiston = 4.0f;
+	float clutch = 0.0f;
+	float mechanicalPress = 8.0f;
+	float encasedFan = 2.0f;
+	float gearshift = 0.0f;
+	float cuckooClock = 1.0f;
+	float brassEncasedShaft = 0.0f;
+	float speedometer = 0.0f;
+	float millstone = 4.0f;
+	float shaft = 0.0f;
+	// "[in Stress Units]"
+	// "Configure how much stress a source can accommodate for."
+	float creativeMotor = 16384.0f;
+	float furnaceEngine = 1024.0f;
+	float handCrank = 8.0f;
+	float windmillBearing = 512.0f;
+	float encasedFanSource = 16.0f;
+	float waterWheel = 16.0f;
 }
