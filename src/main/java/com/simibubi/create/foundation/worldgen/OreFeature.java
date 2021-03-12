313316a52706310af23a00d3f5548206bd2b28c5
smellypengu <43011139+smellypengu@users.noreply.github.com>
2021-03-12 18:47:15 +0100

    Fixed Worldgen and removed Advancement JSON files due to not found errors

--- src/main/java/com/simibubi/create/foundation/worldgen/OreFeature.java
+++ src/main/java/com/simibubi/create/foundation/worldgen/OreFeature.java
@@ -1,12 +1,14 @@
 package com.simibubi.create.foundation.worldgen;
 
-import java.util.Optional;
-
 import net.minecraft.block.Block;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+
+import java.util.Optional;
 
 public class OreFeature {
 
@@ -17,17 +19,21 @@ public class OreFeature {
     protected int clusterCount;
 //    protected YOffset bottom;
 //    protected YOffset top;
+	protected int minHeight;
+	protected int maxHeight;
 
     private final Block block;
     private Biome.Category specificCategory;
 
-    public OreFeature(Block block, int clusterSize, int clusterCount, int aboveBottom, int belowTop) {
+    public OreFeature(Block block, int clusterSize, int clusterCount) {
         this.block = block;
         this.enable = true;
         this.clusterSize = clusterSize;
         this.clusterCount = clusterCount;
 //        this.bottom = YOffset.aboveBottom(aboveBottom);
 //        this.top = YOffset.belowTop(belowTop);
+		this.minHeight = 0;
+		this.maxHeight = 256;
     }
 
     public OreFeature inBiomes(Biome.Category category) {
@@ -38,15 +44,22 @@ public class OreFeature {
     public Optional<ConfiguredFeature<?, ?>> createFeature(Biome biome) {
         if (specificCategory != null && biome.getCategory() != specificCategory)
             return Optional.empty();
+        if (!canGenerate())
+			return Optional.empty();
 
-        return Optional.of(Feature.ORE
+		return Optional.of(Feature.ORE
             .configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, block.getDefaultState(), clusterSize))
-//            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(aboveBottom, belowTop)))
+            //.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(aboveBottom, belowTop)))
+			.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, minHeight, maxHeight)))
             .spreadHorizontally().repeat(clusterCount));
     }
 
     public void setId(String id) {
         this.id = id;
     }
+
+	protected boolean canGenerate() {
+		return minHeight < maxHeight && clusterSize > 0 && enable /*&& !AllConfigs.COMMON.worldGen.disable.get()*/;
+	}
 
 }
