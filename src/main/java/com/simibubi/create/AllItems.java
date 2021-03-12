64310db010d9710dd39102e020404fabe6693601
smellypengu <43011139+smellypengu@users.noreply.github.com>
2021-03-12 21:37:35 +0100

    More items :)

--- src/main/java/com/simibubi/create/AllItems.java
+++ src/main/java/com/simibubi/create/AllItems.java
@@ -17,8 +17,6 @@ import net.minecraft.util.registry.Registry;
 public class AllItems {
 
     //Items
-    public static final Item ZINC_NUGGET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
-    public static final Item ZINC_INGOT = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
     public static final Item REFINED_RADIANCE = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
     public static final Item BELT_CONNECTOR = new BeltConnectorItem(new FabricItemSettings().group(Create.baseCreativeTab));
     public static final GogglesItem GOGGLES = new GogglesItem(new FabricItemSettings().maxCount(1).equipmentSlot(itemStack -> EquipmentSlot.HEAD).group(Create.baseCreativeTab));
@@ -28,14 +26,43 @@ public class AllItems {
 	public static final Item DEFORESTER = new WrenchItem(new FabricItemSettings().maxCount(1).group(Create.baseCreativeTab));
 	public static final Item VERTICAL_GEARBOX = new VerticalGearboxItem(new FabricItemSettings().group(Create.baseCreativeTab));
 
+	public static final Item ZINC_NUGGET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item BRASS_NUGGET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+
+	public static final Item COPPER_SHEET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item BRASS_SHEET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item IRON_SHEET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item GOLDEN_SHEET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item LAPIS_SHEET = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+
+	public static final Item CRUSHED_IRON = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item CRUSHED_GOLD = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item CRUSHED_COPPER = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item CRUSHED_ZINC = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item CRUSHED_BRASS = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+
+	public static final Item ANDESITE_ALLOY = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item COPPER_INGOT = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item ZINC_INGOT = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item BRASS_INGOT = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+
+	public static final Item WHEAT_FLOUR = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item DOUGH = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item CINDER_FLOUR = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item POWDERED_OBSIDIAN = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item ROSE_QUARTZ = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item POLISHED_ROSE_QUARTZ = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item PROPELLER = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item WHISK = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item BRASS_HAND = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+	public static final Item CRAFTER_SLOT_COVER = new Item(new FabricItemSettings().group(Create.baseCreativeTab));
+
     //Block Items
     public static final BlockItem ZINC_ORE = new BlockItem(AllBlocks.ZINC_ORE, new FabricItemSettings().group(Create.baseCreativeTab));
     public static final BlockItem ZINC_BLOCK = new BlockItem(AllBlocks.ZINC_BLOCK, new FabricItemSettings().group(Create.baseCreativeTab));
     public static final BlockItem TILED_GLASS = new BlockItem(AllPaletteBlocks.TILED_GLASS, new FabricItemSettings().group(Create.palettesCreativeTab));
 
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier(Create.ID, "zinc_nugget"), ZINC_NUGGET);
-        Registry.register(Registry.ITEM, new Identifier(Create.ID, "zinc_ingot"), ZINC_INGOT);
         Registry.register(Registry.ITEM, new Identifier(Create.ID, "zinc_ore"), ZINC_ORE);
         Registry.register(Registry.ITEM, new Identifier(Create.ID, "zinc_block"), ZINC_BLOCK);
         Registry.register(Registry.ITEM, new Identifier(Create.ID, "tiled_glass"), TILED_GLASS);
@@ -45,6 +72,38 @@ public class AllItems {
 		Registry.register(Registry.ITEM, new Identifier(Create.ID, "super_glue"), SUPER_GLUE);
 		Registry.register(Registry.ITEM, new Identifier(Create.ID, "builders_tea"), BUILDERS_TEA);
 		Registry.register(Registry.ITEM, new Identifier(Create.ID, "deforester"), DEFORESTER);
+
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "zinc_nugget"), ZINC_NUGGET);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "brass_nugget"), BRASS_NUGGET);
+
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "copper_sheet"), COPPER_SHEET);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "brass_sheet"), BRASS_SHEET);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "iron_sheet"), IRON_SHEET);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "golden_sheet"), GOLDEN_SHEET);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "lapis_sheet"), LAPIS_SHEET);
+
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "crushed_iron_ore"), CRUSHED_IRON);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "crushed_gold_ore"), CRUSHED_GOLD);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "crushed_copper_ore"), CRUSHED_COPPER);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "crushed_zinc_ore"), CRUSHED_ZINC);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "crushed_brass"), CRUSHED_BRASS);
+
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "andesite_alloy"), ANDESITE_ALLOY);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "copper_ingot"), COPPER_INGOT);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "zinc_ingot"), ZINC_INGOT);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "brass_ingot"), BRASS_INGOT);
+
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "wheat_flour"), WHEAT_FLOUR);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "dough"), DOUGH);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "cinder_flour"), CINDER_FLOUR);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "powdered_obsidian"), POWDERED_OBSIDIAN);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "rose_quartz"), ROSE_QUARTZ);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "polished_rose_quartz"), POLISHED_ROSE_QUARTZ);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "propeller"), PROPELLER);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "whisk"), WHISK);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "brass_hand"), BRASS_HAND);
+		Registry.register(Registry.ITEM, new Identifier(Create.ID, "crafter_slot_cover"), CRAFTER_SLOT_COVER);
+
 		//BuiltinItemRendererRegistry.INSTANCE.register(WRENCH, WrenchItemRenderer::render);
 		//BuiltinItemRendererRegistry.INSTANCE.register(DEFORESTER, DeforesterItemRenderer::render);
     }
